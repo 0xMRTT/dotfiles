116e71b62f45570f2c3afd10450a574239e104bc
0xMRTT <0xMRTT@tuta.io>
2022-05-31 19:46:10 +0200

    Add dot_zshrc.d/alias.sh

---
+++ dot_zshrc.d/alias.sh
@@ -0,0 +1,19 @@
+# Alias
+alias ls='exa --icons'                                                          # ls
+alias l='exa -lbF --git --icons'                                                # list, size, type, git
+alias ll='exa -lbGF --git --icons --icons'                                      # long list
+alias llm='exa -lbGd --git --sort=modified --icons'                             # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale --icons'   # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --icons'  # all + extended list
+											
+# specialty views
+alias lS='exa -1 --icons'                                                       # one column, just names
+alias lt='exa --tree --level=2 --icons'                                         # tree
+alias tree="lt"
+
+# pacman
+alias autoremove='yay -R $(yay -Qtqd)'
+
+# chezmoi
+alias dot="chezmoi add"
+alias dot-apply="chezmoi apply"
